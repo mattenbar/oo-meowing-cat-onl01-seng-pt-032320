bb2c8c51710140f4b9770a6449caee6fa2f6581c
matthew enbar <matthew.enbar@gmail.com>
2020-03-31 18:29:58 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,14 @@
-## code your solution here. 
+## code your solution here.
+class Cat
+  #macro that creates both getter and setter for instance variable
+  # attr_reader for just getters
+  # attr_writer for just setters
+  attr_accessor :name
+  attr_writer :meow
+
+# this is the getter for meow but since it has an output
+# we write it seperately
+  def meow
+    puts "meow!"
+  end
+end
